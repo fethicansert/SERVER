9e49cb06951d0ab58089a0d5f3c5374259f3bd7b
fethicansert <77362516+fethicansert@users.noreply.github.com>
2022-01-01 23:23:22 +0300

    olacak hocam :D

--- server.cpp
+++ server.cpp
@@ -7,20 +7,25 @@ using namespace std;
 
 int main()
 {
-	
-	
 	char commandline[21];
 	char line[100];
 	char command[10];
 	char name[21];
+	char name1[21];
 	char password[21];
 	char t_name[21];
 	char t_password[21];
+	bool enter=true;
 	char *sp;
 	FILE *file;
 	file=fopen("12.txt","r");
+	
+	while(enter)
+	{
+	
 	printf("Enter Command : ");
-    scanf("%20[^\n]",&commandline);
+	memset( &commandline, '\0', sizeof(commandline) );
+    scanf("%20[^\n]",&commandline); //%20[^\n]
 	sp=strtok(commandline," ");
 	strcpy(command,sp);
 	if(strcmp(command,"USER")==0)
@@ -30,37 +35,44 @@ int main()
 		sp=strtok(NULL," ");
 		strcpy(password,sp);
 		
-		
 		while(fgets(line,100,file)!=NULL)
     	{
     		
     		sp=strtok(line,":");
     		strcpy(t_name,sp);
-    		sp=strtok(NULL,"\t");
+    		sp=strtok(NULL,"\n");
     		strcpy(t_password,sp);
     		
+    		//printf("%s %s %s %s",name,password,t_name,t_password);
+    		//&& strcmp(password,t_password)==0
+    		//   strcmp(name,t_name)==0
     		
-    		if(strcmp(name,t_name)==0 && strcmp(password,t_password))
+    		if(strcmp(password,t_password)==0 && strcmp(name,t_name)==0)
     		{
-		
-    			
     			printf("200  User %s  has grated to access",name);
+    			scanf("%s",name1);
+    			enter=false;
     			break;
 			}
-    		
-    		
-    
-		}
-		
-
+			
+    	
+		}			
 	}
 	else if(strcmp(command,"QUIT")==0)
 	{
 		printf("TRUE COMMAND");
 	}
-	
-	
-
+	else
+	{
+		
+		printf("WRONG COMMAND");
+	}
 	
 }
+	fclose(file);
+	
+	
+	
+}
+
 
